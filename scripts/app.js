5f33b20c410a5ae61975d210b81f2a84fe395454
jwilson <jwilson@dealeron.com>
2017-08-19 16:20:38 -0400

    Adding some interaction methods.

--- scripts/app.js
+++ scripts/app.js
@@ -13,6 +13,10 @@ myApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
       $scope.page = pageName;
   }
 
+  //Array of adventures
+  $scope.adventures = [];
+  $scope.currentAdventure = {};
+
   $scope.createAdventure = function() {
       $http.get({
         method: 'GET',
@@ -25,4 +29,44 @@ myApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
         }
       );
   }
+
+  $scope.getAdventure = function(id) {
+      $http.get({
+        method: 'GET',
+        url: '/api/room/' + id
+      }).then(function successCallback(response) {
+          $scope.currentAdventure = response;
+          $scope.changePage('current');
+        }, function errorCallback(response) {
+          alert('Error Locating Room');
+        }
+      );
+  }
+
+  $scope.getAdventures = function() {
+      $http.get({
+        method: 'GET',
+        url: '/api/rooms/'
+      }).then(function successCallback(response) {
+          $scope.adventures = response;
+          $scope.changePage('ongoing');
+        }, function errorCallback(response) {
+          alert('Error Locating Rooms');
+        }
+      );
+  }
+
+  $scope.addDriver = function(id) {
+      $http.get({
+        method: 'GET',
+        url: '/api/room/' + id
+      }).then(function successCallback(response) {
+          $scope.currentAdventure = response;
+          $scope.changePage('current');
+        }, function errorCallback(response) {
+          alert('Error Locating Room');
+        }
+      );
+  }
+
 }]);
