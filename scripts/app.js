720d41bd8cb79d02f0301cc90cfc70971bd5b7de
Markese Dixon <mdixon@ad.dealeron.com>
2017-08-19 16:13:51 -0400

    first method

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('MainCtrl', ['$scope', function($scope) {
+myApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.user = {
       firstName: '',
       lastName: '',
@@ -11,5 +11,18 @@ myApp.controller('MainCtrl', ['$scope', function($scope) {
 
   $scope.changePage = function(pageName) {
       $scope.page = pageName;
+  }
+
+  $scope.createAdventure = function() {
+      $http.get({
+        method: 'GET',
+        url: '/api/room'
+      }).then(function successCallback(response) {
+          $scope.adventures.push(response)
+          $scope.changePage('ongoing');
+        }, function errorCallback(response) {
+          alert('Error Creating Room');
+        }
+      );
   }
 }]);
