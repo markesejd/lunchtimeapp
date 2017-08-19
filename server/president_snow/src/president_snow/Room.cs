fddd5c56df812ff8ccf209d1685827016ffe83fe
jwilson <jwilson@dealeron.com>
2017-08-19 12:02:02 -0400

    Adding simple server

---
+++ server/president_snow/src/president_snow/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace president_snow
+{
+  public class Room
+  {
+    public string Name { get; set; }
+
+    public VotableItemList Times { get; set; }
+    public VotableItemList Venues { get; set; }
+    public List<string> Drivers { get; set; }
+
+    public Room(string name)
+    {
+      this.Name = name;
+      this.Times = new VotableItemList();
+      this.Venues = new VotableItemList();
+      this.Drivers = new List<string>();
+    }
+  }
+}
